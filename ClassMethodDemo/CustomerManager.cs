fba18e8a48f3ac7f0bd8150c588bc0fb396f4ccd
Recep Pazarlı <receppazarli99@hotmail.com>
2021-05-19 16:23:53 +0300

    Engin Demiroğ Yazılım kampı 3. Ödev

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+" "+customer.LastName+ "s customer information has been saved");
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + "s customer information has been updated");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + "s customer information was deleted");
+        }
+
+        public void GelAll(Customer customer)
+        {
+            Console.WriteLine(
+              "CustomerId: " +  customer.CustomerId+"\n" +
+              "FirstName: " + customer.FirstName+ "\n"+
+              "LastName: " + customer.LastName+ "\n"+
+              "IdentityNumber: " + customer.IdentityNumber+ "\n"+
+              "Phone: " + customer.Phone+ "\n"+
+              "Mail: " + customer.Mail+ "\n"+
+              "City: " + customer.City+ "\n" +
+              "Country: " + customer.Country+ "\n" +
+              "Address: " + customer.Address);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                CustomerId = 1,
+                FirstName = "Recep",
+                LastName = "Pazarlı",
+                IdentityNumber = "12345678912",
+                Phone = "02133211232",
+                Mail = "customer@gmail.com",
+                City = "Bursa",
+                Country = "İnegöl",
+                Address = "Taksi yanı no:6"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            Console.WriteLine("******************************************");
+            customerManager.Update(customer1);
+            Console.WriteLine("******************************************");
+            customerManager.Delete(customer1);
+            Console.WriteLine("******************************************");
+            customerManager.GelAll(customer1);
+
+        }
+
+
+
+
+    }
+}
+
